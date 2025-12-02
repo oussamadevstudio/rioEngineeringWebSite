af0f487785407cdc6bfbaa0c8772042b91592716
oussamadev.studio <oussamadev.studio@gmail.com>
2025-12-02 22:03:42 +0100

    first commit

---
+++ js/sidebar.js
@@ -0,0 +1,11 @@
+const showSidebar = () => {
+    const sideBar = document.querySelector("aside")
+    sideBar.style.display = "flex"
+
+}
+
+const hideSideBar = () => {
+    const sideBar = document.querySelector("aside")
+    sideBar.style.display = "none"
+
+}
