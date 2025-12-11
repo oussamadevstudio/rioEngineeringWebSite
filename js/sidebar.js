400572b6520c5063c1103077031f0e17567acb2c
oussamadev.studio <oussamadev.studio@gmail.com>
2025-12-11 10:42:31 +0100

    little changes

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,11 +1,16 @@
 const showSidebar = () => {
     const sideBar = document.querySelector("aside")
+    const sideBarIcon = document.querySelector("nav")
+
     sideBar.style.display = "flex"
+    sideBarIcon.style.visibility = "hidden"
 
 }
 
 const hideSideBar = () => {
     const sideBar = document.querySelector("aside")
-    sideBar.style.display = "none"
+    const sideBarIcon = document.querySelector("nav")
 
+    sideBar.style.display = "none"
+    sideBarIcon.style.visibility = "visible"
 }
